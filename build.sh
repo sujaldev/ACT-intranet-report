6b9072a67938195ba0fc42bb494e54881b0230ad
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-19 09:01:21 +0530

    fix build script not using images

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ update_deployment in_progress "https://pdf.sujal.dev/act/report.pdf"
 
 # Build PDF
 mkdir -p ./build/act
-pdflatex -interaction=nonstopmode -output-format=pdf -output-directory=./build/act -shell-escape ./src/report.tex
-cd ./build/act || exit
+cd src/ || exit
+pdflatex -interaction=nonstopmode -output-format=pdf -output-directory=../build/act -shell-escape ./report.tex
+cd ../build/act || exit
 
 # Clean up build files
 find ./* ! -name "report.pdf" -delete
